bb4e6543d597d50d7bbffb1f8a4f5de658d64cf4
ybotspawn <ybotspawn@gmail.com>
2015-07-03 17:41:17 -0400

    Updated local copies from sneakernet

--- Fibrary.fs
+++ Fibrary.fs
@@ -4,24 +4,98 @@ open System.DirectoryServices
 open System.DirectoryServices.ActiveDirectory
 open System
 
+// fix my default arguments
+// http://stackoverflow.com/questions/15988913/why-default-arguments-in-f-fsharpoptiont-are-reference-types
 module ActiveDirectory =
-    type ActiveDirectoryOptionalArguments() =
-        static let defaultDomainRoot() = "place.holder.for.optional.arg.stuff.later.com"
-    
-    let getCurrentSite =
-        System.DirectoryServices.ActiveDirectory.Domain.GetCurrentDomain().Name
+//    let __getAllDomainControllers =
+//        
+//        printfn "getting all domain controllers"
+//         private List<DomainController> _getDCLoop()
+//        {
+//            List<DomainController> dcs = new List<DomainController>();
+//            while (true)
+//            {
+//                try
+//                {
+//                    var dc = DomainController.FindOne(new DirectoryContext(DirectoryContextType.Domain, SITE));
+//                    if (dcs.Contains(dc))
+//                        break;
+//                    else
+//                        dcs.Add(dc);                    
+//                }
+//                catch (Exception aoe)
+//                {
+//                    // log warning, probably ok to ignore
+//                }
+//            }
+//
+//            return dcs;
+//        }
+// try
+//            {
+//                DCC = DomainController.FindAll(new DirectoryContext(DirectoryContextType.Domain, SITE));
+//                var dcs = (from DomainController dc in DCC select dc).ToList();
+//                return dcs;
+//            }
+//            catch (ActiveDirectoryOperationException aoe)
+//            {
+//                return null;
+//            }
 
-    let getDomainRoot (globalArg : bool) =
-        if globalArg then new DirectoryEntry("LDAP://GlobalCatalog")
-        else new DirectoryEntry("LDAP://root ou")
 
-    let rec getObjectBySAMAccount (samAccountName : string) =
-        let searcher = new DirectorySearcher(getDomainRoot(false), String.Format("(sAMAccountName={0})", samAccountName))
-        match searcher.FindOne() with
-        | null -> getObjectBySAMAccount("Default or Builtin account")
-        | _ -> searcher.FindOne().GetDirectoryEntry()
+//Construction
+let getRootOrganizationUnit (domain : string) = // how do we want to handle subou overrides? let the implementer override by building their directoryentry directly
+    // check a given value somewhere for the override value?
+    new DirectoryEntry(String.Format("LDAP://DC={0}", domain.Replace(".", ",DC=")))
+
+let getCurrentSite =
+    System.DirectoryServices.ActiveDirectory.Domain.GetCurrentDomain().Name
+
+let getGlobalCatalog (domain : string) =
+    new DirectoryEntry(String.Format(@"GC://{0}", getCurrentSite))
+    //let gc = GlobalCatalog.FindOne(new DirectoryContext(DirectoryContextType.Domain))
+
+let getDomainRoot (globalArg : bool) =
+    if globalArg then getGlobalCatalog(getCurrentSite)
+    else getRootOrganizationUnit(getCurrentSite)
+
+let getAllDomainControllers =
+    printfn "Getting all dc's"
+
+// Properties
+let rec getObjectBySAMAccount (samAccountName : string) = // fix this and make it an optional argument
+    let searcher = new DirectorySearcher( getDomainRoot(false), String.Format("(sAMAccountName={0})", samAccountName))
+    match searcher.FindOne() with
+    | null -> new DirectoryEntry() // return an empty DE
+    | _ -> searcher.FindOne().GetDirectoryEntry()
+
+let userDisabled (user, ad) = 
+    printfn "Return null"
     
-    let getGlobalCatalog (domain : string):
-        printfn "Get LDAP://globalcatalog"
-    let getRootOrganizationalUnit (domain : string, subOU : string): // how do we want to handle subou overrides
-        printfn "Get LDAP://root ou"
+let getIntProperty ( objectParam : string, property: string) =
+    printfn "string property"
+let getStringProperty ( directoryEntry : DirectoryEntry, property: string) =
+    let prop = directoryEntry.Properties.[property].Value.ToString()
+    printfn "string property"
+let getDateProperty ( samAccountName :string, property: string ) = // change this to take a type in as well to determine the kind of object param we are passing, ie sam, de, etc
+    printfn "lastpwdset"
+//    public string getStringProperty(String distinguishedName, String propertyValue)
+//    {
+//        if (!distinguishedName.StartsWith(@"LDAP://"))
+//            distinguishedName = String.Format(@"LDAP://{0}", distinguishedName);
+//        String property = "undefined";
+//        DirectoryEntry tempDe = null;
+//        try
+//        {
+//            tempDe = new DirectoryEntry(distinguishedName);
+//            property = tempDe.Properties[propertyValue].Value.ToString();
+//
+//        }
+//        catch (Exception e)
+//        {
+//            // log event here
+//        }
+//        tempDe.Close();
+//        return property;
+//    }
+    
